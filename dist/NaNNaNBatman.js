2fc36390657af1e78176e1628f61c4d6727ff866
Patrick Pietens <hello@patrickpietens.com>
2016-04-01 16:38:25 +0200

    Fixed build errors

--- dist/NaNNaNBatman.js
+++ dist/NaNNaNBatman.js
@@ -1,1 +1,1 @@
-!function(e,t){"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?module.exports=t():e.NaNNaNBatman=t()}(this,function(){"use strict";return select});
+!function(n,t){"function"==typeof define&&define.amd?define([],t):"object"==typeof exports?module.exports=t():n.NaNNaNBatman=t()}(this,function(){"use strict";var n=function(){return Array(16).join(NaN)+" Batman!"};return n});
